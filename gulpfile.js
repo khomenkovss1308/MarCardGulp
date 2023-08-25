2c1871d25771b6ba0152312d886668cb027d32a4
ITG-SOFT <morozov@itg-soft.by>
2023-08-25 10:22:19 +0300

    fix gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -36,8 +36,7 @@ function scripts() {
   return src([
     'app/js/main.js',
   ])
-    // .pipe(concat('main.min.js'))
-    .pipe(uglify())
+    // .pipe(uglify())
     .pipe(dest("app/js"))
     .pipe(browserSync.stream());
 }
