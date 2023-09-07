de51f0450f917cbdc8d3d8f6bee567c59c2e1c3f
ITG-SOFT <morozov@itg-soft.by>
2023-09-07 21:01:46 +0300

    update mileage-cars-card__list (list info car)

---
+++ app/js/sub-file/mileage-cars-list.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const lists = document.querySelectorAll('.mileage-cars-card__list');
+
+    lists.forEach((list) => {
+        const items = list.querySelectorAll('.mileage-cars-card__list__item');
+        const hiddenItems = list.querySelectorAll('.mileage-cars-card__list__item.hidden');
+        const toggleButton = list.querySelector('.toggle-btn'); // Измените на класс кнопки внутри списка
+
+        let isExpanded = false;
+
+        toggleButton.addEventListener('click', () => {
+            hiddenItems.forEach((item) => {
+                if (isExpanded) {
+                    item.style.display = 'none';
+                } else {
+                    item.style.display = 'list-item';
+                }
+            });
+
+            if (isExpanded) {
+                toggleButton.innerHTML = `
+                    <img src="images/icons/default/plus.svg" alt="">
+                    <span>Еще <span class="blue">+${hiddenItems.length}</span></span>
+                `;
+            } else {
+                toggleButton.innerHTML = `<span class="blue">Свернуть<span>`;
+            }
+
+            isExpanded = !isExpanded;
+        });
+    });
+});
