bc368584a099c774a8e49a87e5d37061482bc8e5
ITG-SOFT <morozov@itg-soft.by>
2023-09-18 21:27:28 +0300

    update toggle text

--- app/js/swiper/thumbs.js
+++ app/js/swiper/thumbs.js
@@ -1,38 +1,25 @@
-let swiper1 = new Swiper(".swiper-thumbs-1", {
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-});
-let swiper2 = new Swiper(".swiper-thumbs-1-top", {
-    loop: true,
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-        swiper: swiper1,
-    },
-});
-let swiper3 = new Swiper(".swiper-thumbs-2", {
-    loop: true,
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesProgress: true,
-});
-let swiper4 = new Swiper(".swiper-thumbs-2-top", {
-    loop: true,
-    spaceBetween: 10,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-        swiper: swiper3,
-    },
-});
+document.querySelectorAll(".appearance__slider").forEach(slider => {
+    const sliderName = slider.getAttribute("data-slider");
+    const thumbsTop = slider.querySelector(".swiper-thumbs-top");
+    const thumbs = slider.querySelector(".swiper-thumbs");
 
+    const thumbsSwiper = new Swiper(thumbs, {
+        loop: true,
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+    });
 
+    new Swiper(thumbsTop, {
+        loop: true,
+        spaceBetween: 10,
+        navigation: {
+            nextEl: thumbsTop.querySelector(".swiper-button-next"),
+            prevEl: thumbsTop.querySelector(".swiper-button-prev"),
+        },
+        thumbs: {
+            swiper: thumbsSwiper,
+        },
+    });
+});
