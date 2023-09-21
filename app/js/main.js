3e13d8a51cd51ee4681ed8fb364c44f094a36993
ITG-SOFT <morozov@itg-soft.by>
2023-09-21 20:13:10 +0300

    add btn-up

--- app/js/main.js
+++ app/js/main.js
@@ -234,10 +234,45 @@ const compareContentWidth = () => {
     }
 }
 
+const appearanceBtnup = () => {
+    const btnUp = {
+        el: document.querySelector('.btn-up'),
+        show() {
+            // удалим у кнопки класс btn-up_hide
+            this.el.classList.remove('btn-up_hide');
+        },
+        hide() {
+            // добавим к кнопке класс btn-up_hide
+            this.el.classList.add('btn-up_hide');
+        },
+        addEventListener() {
+            // при прокрутке содержимого страницы
+            window.addEventListener('scroll', () => {
+                // определяем величину прокрутки
+                const scrollY = window.scrollY || document.documentElement.scrollTop;
+                // если страница прокручена больше чем на 400px, то делаем кнопку видимой, иначе скрываем
+                scrollY > 400 ? this.show() : this.hide();
+            });
+    
+            // при нажатии на кнопку .btn-up
+            this.el.onclick = () => {
+                // переместим в начало страницы
+                window.scrollTo({
+                    top: 0,
+                    left: 0,
+                    behavior: 'smooth'
+                });
+            }
+        }
+    }
+    btnUp.addEventListener();
+}
+
 document.addEventListener('DOMContentLoaded', () => {
     videPlayer(".video__container", ".video");
 
     toggleText();
+    appearanceBtnup();
 
     toggleSubMenu();
     sidebarFunctioning();
